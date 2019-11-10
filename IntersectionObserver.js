7eb5485c2ce7f6e9804d1385e72253f0dcc9a1ba
Kaloyan Ivanov <kivanov@gmail.com>
2019-11-10 17:16:56 +0200

    .

---
+++ IntersectionObserver.js
@@ -0,0 +1,14 @@
+const carousel = document.getElementById('carousel');
+let isLeaving = false;
+let observer = new IntersectionObserver(function(entries) {
+  entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          isLeaving = true;
+          entry.target.carousel();
+        } else if (isLeaving) {
+          isLeaving = false;
+          entry.target.carousel('dispose');
+        }
+    });
+})
+observer.observe(carousel);
