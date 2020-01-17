49fab8a1a9286c6f51153dedbc63d7702ef152f6
kaloyanivanov <kivanov@gmail.com>
2020-01-17 17:50:12 +0200

    .

--- MaaS Hit Log.sql
+++ MaaS Hit Log.sql
@@ -1,4 +1,3 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
 SELECT [TimeStamp]
       ,FirstName + ' ' + LastName As FullName
 	  ,D.UserID
@@ -19,3 +18,42 @@ SELECT *
 FROM View_CMS_Tree_Joined INNER JOIN oneIM_Campaign ON View_CMS_Tree_Joined.DocumentForeignKeyValue = oneIM_Campaign.CampaignID
 WHERE (ClassName = 'oneIM.Campaign')
 
+
+
+
+/*DEFAULT*/
+SELECT [TimeStamp]
+      ,[FirstName] + ' ' + [LastName] AS FullName
+	  ,D.UserID
+      ,[FileGUID]
+	  ,[AttachmentDocumentID]
+	  ,[AttachmentName]	  
+	  ,[DocumentID]
+	  ,[DocumentName]
+	  ,RANK() OVER (PARTITION BY DocumentName ORDER BY FileGUID) FileRank
+  FROM CMS_Attachment AS A 
+  INNER JOIN Analytics_FileDownloads AS D ON A.AttachmentGUID = D.FileGUID
+  INNER JOIN CMS_User AS U ON D.UserID = U.UserID
+  INNER JOIN View_CMS_Tree_Joined AS T ON T.DocumentID = A.AttachmentDocumentID
+  WHERE SiteID = 2
+
+  /*HIT COUNT*/
+
+SELECT DISTINCT FileGUID
+	  ,[AttachmentName]	  
+	  ,[DocumentID]
+	  ,[DocumentName]
+	  ,COUNT(FileGUID) HitCount
+	  ,CampaignValidFrom
+  ,NodeParentID
+  ,CampaignImage
+  ,CampaignName
+  FROM CMS_Attachment AS A 
+  INNER JOIN Analytics_FileDownloads AS D ON A.AttachmentGUID = D.FileGUID
+  INNER JOIN CMS_User AS U ON D.UserID = U.UserID
+  INNER JOIN View_CMS_Tree_Joined AS T ON T.DocumentID = A.AttachmentDocumentID
+  INNER JOIN oneIM_Campaign C ON T.DocumentForeignKeyValue = C.CampaignID
+  WHERE SiteID = 2
+  --AND DocumentID = 89003
+  GROUP BY FileGUID, AttachmentName, DocumentID, DocumentName,CampaignValidFrom,NodeParentID,CampaignImage,CampaignName
+  ORDER BY HitCount DESC
