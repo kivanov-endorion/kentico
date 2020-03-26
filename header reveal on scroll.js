be68d11aacb2efde49e30abfa451825e42d2bc1b
Ivanov <Kaloyan.Ivanov@ingrammicro.com>
2020-03-26 15:33:34 +0200

    .

---
+++ header reveal on scroll.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 200) { 
+              $('.navbar').addClass('bg-primary');
+          } else {
+              $('.navbar').removeClass('bg-primary');
+          }
+        });
+  
+  $(document).on('click', '.cta', function () {
+      $(this).toggleClass('active')
+  })
+});
