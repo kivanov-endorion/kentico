6f684de2f7a0809f4ed55896f4ffdc9e99891203
Kaloyan Ivanov <kivanov@gmail.com>
2020-06-20 13:10:12 +0300

    .

---
+++ transformations/ProductSeries-Card-in-a-row-long-desc.cs
@@ -0,0 +1,3 @@
+<h6 class="initialism mb-4" id="{% Documents[NodeAliasPath].NodeAlias #%}">{% Documents[NodeAliasPath].DocumentName #%}</h6>
+{% ArticleText %}
+{% Documents[NodeAliasPath].Children.ClassNames("oneIM.Product").WithAllData.ApplyTransformation("oneIM.ContainerB4.Product-Card-in-a-row-long-desc") #%}
