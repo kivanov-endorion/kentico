328d407adba026d00e564bc1ab608bc8bd648ba0
Ivanov <Kaloyan.Ivanov@ingrammicro.com>
2020-07-27 17:51:44 +0300

    .

--- cookies.cs
+++ cookies.cs
@@ -19,3 +19,34 @@
 // 200 - visitor level
 // 1000 - all cookies
 // not registered cookies (third part) - considered 200 level
+
+
+// Visibility Condition
+{% !domain.Matches("([a-z]){2,9}(-)(ingrammicro)(-asia)?(-)?(eu)?") && CurrentSite.SiteID.ToString().InList("4;19;23;26;27;32;37;38;58;63;71;72".Split(";")) && ViewMode=="LiveSite" #%}
+
+
+// B4 Cookies (old)
+{%if (CurrentSite.SiteID==23 || CurrentSite.SiteID==26 || CurrentSite.SiteID==27 || CurrentSite.SiteID==32 || CurrentSite.SiteID==38 || CurrentSite.SiteID==59 || CurrentSite.SiteID==62 || CurrentSite.SiteID==63 || CurrentSite.SiteID==71) { 
+    if(ViewMode=="LiveSite" || ViewMode=="Unknown"){
+       return true;
+     } else {
+       return false;
+     }
+} 
+#%}
+
+
+// B4 Cookies (legacy)
+
+{%if (CurrentSite.SiteID==23 || CurrentSite.SiteID==26 || CurrentSite.SiteID==27 || CurrentSite.SiteID==32 || CurrentSite.SiteID==38 || CurrentSite.SiteID==59 || CurrentSite.SiteID==62 || CurrentSite.SiteID==63 || CurrentSite.SiteID==71) { return false;} else { 
+    if(ViewMode=="LiveSite" || ViewMode="Unknown"){
+       if(Cookies["CMSCookieLevel"]=="1000"){
+         return false;
+       } else { // if CMSCookieLevel is different from 1000 or not set show bannder
+         return true;
+       }
+  } else { // hiden on all other ViewModes
+     return false;
+  }
+} 
+#%}
