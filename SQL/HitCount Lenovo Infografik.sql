fe1f1faf206d1d4c581e6a0cceead5a5ea710221
Ivanov, Kaloyan <kaloyan.ivanov@ingrammicro.com>
2020-11-18 14:37:38 +0200

    .

--- SQL/HitCount Lenovo Infografik.sql
+++ SQL/HitCount Lenovo Infografik.sql
@@ -4,3 +4,14 @@ FROM [KENTICO].[dbo].[Analytics_FileDownloads] D
     ON D.UserID = U.UserID
 WHERE D.SiteID = '4'
     AND D.Referrer LIKE '%/lenovo-modern-workplace%'
+
+
+
+SELECT F.Kundennummer, F.Vorname, F.Email, D.TimeStamp
+FROM [KENTICO].[dbo].[Analytics_FileDownloads] D
+    LEFT JOIN [KENTICO].[dbo].[CMS_User] U
+    ON D.UserID = U.UserID
+	LEFT JOIN [KENTICO].[dbo].[Form_de_ingrammicro_eu_LenovoModernWorkplace] F
+	ON CONCAT(ISNULL(U.imBranchNbr,0), ISNULL(U.imCustomerNbr,0)) = F.Kundennummer
+WHERE D.SiteID = '4'
+    AND D.Referrer LIKE '%/lenovo-modern-workplace%'
