361fa561207cb9f1ad26549f0fc4c0d46854ba9b
Ivanov, Kaloyan <kaloyan.ivanov@ingrammicro.com>
2020-11-26 18:15:22 +0200

    .

---
+++ de-inside.js
@@ -0,0 +1,23 @@
+<script type="text/javascript">
+$(document).ready(function(){
+  	$('.xmasCarousel').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        draggable: true,
+        pauseOnHover: true
+	})
+});
+</script>
+
+
+<div class="xmasCarousel">
+<p><img class="img-fluid" src="/getattachment/Home/HR-Tools/weihnachtsaktion-2020/Image_Slide1.jpg.aspx" alt></p>
+<p><img class="img-fluid" src="/getattachment/Home/HR-Tools/weihnachtsaktion-2020/Image_Slide2.jpg.aspx" alt></p>
+<p><img class="img-fluid" src="/getattachment/Home/HR-Tools/weihnachtsaktion-2020/Image_Slide3.jpg.aspx" alt></p>
+<p><img class="img-fluid" src="/getattachment/Home/HR-Tools/weihnachtsaktion-2020/Image_Slide4.jpg.aspx" alt></p>
+<p><img class="img-fluid" src="/getattachment/Home/HR-Tools/weihnachtsaktion-2020/Image_Slide5.jpg.aspx" alt></p>
+</div>
