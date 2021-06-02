6f04a23c9756328e35bc20118218a46682fe638f
Ivanov, Kaloyan <kaloyan.ivanov@ingrammicro.com>
2021-06-02 10:40:21 +0300

    .

--- snippets-webparts.cs
+++ snippets-webparts.cs
@@ -3,8 +3,8 @@ using System.Text.RegularExpressions;
 
 int currentCookieLevel = GetCurrentCookieLevel();
 bool UseOneTrust = SettingsKeyInfoProvider.GetBoolValue(SiteContext.CurrentSiteName + ".UseOneTrustCookieConsent");
-string dashdomain = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
-string pattern = @"(-ingrammicro)(-asia)?(-)(eu)?$";
+string dashdomain = Request.Url.Host;
+string pattern = @"^[a-z]{2,20}(-ingrammicro)(-asia)?(-)(eu)?$";
 Regex rgx = new Regex(pattern);
 
-if (UseOneTrust && rgx.IsMatch(dashdomain))
+if (UseOneTrust && !rgx.IsMatch(dashdomain))
