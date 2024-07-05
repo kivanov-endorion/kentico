65bc4fb16938022c4339c5035731190c52db1a56
Kaloyan <k.ivanov@gmail.com>
2024-07-05 16:23:32 +0300

    .

--- SQL/Pros Services Pages Related Contacts and Categories.sql
+++ SQL/Pros Services Pages Related Contacts and Categories.sql
@@ -59,3 +59,41 @@ T.DocumentName,
       ,[TargetCustomers]
 
 ORDER BY NodeAliasPath, DocumentName
+
+
+==============
+
+
+USE KENTICO
+
+SELECT 
+	  T.[DocumentName]
+	  ,PARSENAME(REPLACE(SUBSTRING(C.CategoryNamePath, 0, CHARINDEX('/', C.CategoryNamePath, CHARINDEX('/', C.CategoryNamePath, CHARINDEX('/', C.CategoryNamePath) + 1) + 1)), '/', '.'), 1) ParentCategory
+	  ,STRING_AGG(C.CategoryDisplayName, ';') Category
+	  
+
+FROM view_cms_tree_joined T 
+
+LEFT JOIN CMS_DocumentCategory DC 
+ON T.DocumentID = DC.DocumentID
+
+
+LEFT JOIN [KENTICO].[dbo].[CMS_Category] C
+ON DC.CategoryID = C.CategoryID
+
+WHERE
+	T.NodeSiteID = 74
+	AND T.NodeAliasPath like '/Professional-Services%'
+
+Group by
+T.DocumentName, 
+	   [NodeAliasPath]
+	  ,DC.[CategoryID]
+	  ,C.[CategoryID]
+	  ,C.CategoryParentID
+	  ,C.CategoryDisplayName
+	  ,T.DocumentID
+	  ,C.CategoryNamePath
+	  ,C.CategoryLevel
+
+ORDER BY DocumentName, ParentCategory
