f97ad3148d11b18f7e977499dcc9baaabfef2336
Kaloyan <k.ivanov@gmail.com>
2024-07-11 13:35:16 +0300

    .

--- SQL/Pros Services Pages Related Contacts and Categories.sql
+++ SQL/Pros Services Pages Related Contacts and Categories.sql
@@ -1,5 +1,3 @@
-USE KENTICO
-
 SELECT 
 	  T.[DocumentName]
 	  ,T.[NodeAliasPath]	
@@ -9,14 +7,11 @@ SELECT
       ,[CostsInternal]
       ,[HowToOrderInternal]
       ,[DeliverModel]
-      ,[Contact]
       ,[InfoInternal]
-      ,[RelatedServices]
       ,[InfoPartner]
       ,[InfoEndCustomer]
       ,[TargetCustomers]
-	  ,(SELECT DocumentName FROM view_cms_tree_joined WHERE NodeID = R.RightNodeID AND R.RelationshipNameID = 4) ContactName
-	  --,(SELECT STRING_AGG(DocumentName, ', ') FROM view_cms_tree_joined WHERE NodeID = R.RightNodeID AND R.RelationshipNameID = 3) RelService
+	  ,(SELECT STRING_AGG(R.RightNodeName, ';') FROM [KENTICO].[dbo].[View_CMS_Relationship_Joined] R WHERE T.NodeID = R.LeftNodeID AND R.RelationshipNameID = 4) AS Contact
 	  ,STRING_AGG(C.CategoryDisplayName, ';') Category
 
 FROM view_cms_tree_joined T 
@@ -24,8 +19,9 @@ FROM view_cms_tree_joined T
 LEFT JOIN [KENTICO].[dbo].[oneIMProfServicesCatalog_1] PC
 ON T.DocumentForeignKeyValue = PC.ProfServicesCatalogID
 
-LEFT JOIN [KENTICO].[dbo].[CMS_Relationship] R
-ON R.LeftNodeID = T.NodeID
+LEFT JOIN [KENTICO].[dbo].[View_CMS_Relationship_Joined] R
+		--ON T.NodeID = R.LeftNodeID
+		ON T.DocumentForeignKeyValue = R.RelationshipID
 
 LEFT JOIN CMS_DocumentCategory DC 
 ON T.DocumentID = DC.DocumentID
@@ -37,63 +33,20 @@ ON DC.CategoryID = C.CategoryID
 WHERE
 	T.NodeSiteID = 74
 	AND T.NodeAliasPath like '/Professional-Services%'
-	--AND T.ClassName = 'oneIM.ProfServicesCatalog'
-	--AND (R.RelationshipNameID = 4)
 
-Group by
-T.DocumentName, 
-	   [NodeAliasPath]
-	  ,[RightNodeID]
-	  ,[RelationshipNameID]
+GROUP BY
+T.DocumentName
+,T.NodeID
+	  ,[NodeAliasPath]
 	  ,[Description]
       ,[UseCases]
       ,[Exclusions]
       ,[CostsInternal]
       ,[HowToOrderInternal]
       ,[DeliverModel]
-      ,[Contact]
       ,[InfoInternal]
-      ,[RelatedServices]
       ,[InfoPartner]
       ,[InfoEndCustomer]
       ,[TargetCustomers]
 
 ORDER BY NodeAliasPath, DocumentName
-
-
-==============
-
-
-USE KENTICO
-
-SELECT 
-	  T.[DocumentName]
-	  ,PARSENAME(REPLACE(SUBSTRING(C.CategoryNamePath, 0, CHARINDEX('/', C.CategoryNamePath, CHARINDEX('/', C.CategoryNamePath, CHARINDEX('/', C.CategoryNamePath) + 1) + 1)), '/', '.'), 1) ParentCategory
-	  ,STRING_AGG(C.CategoryDisplayName, ';') Category
-	  
-
-FROM view_cms_tree_joined T 
-
-LEFT JOIN CMS_DocumentCategory DC 
-ON T.DocumentID = DC.DocumentID
-
-
-LEFT JOIN [KENTICO].[dbo].[CMS_Category] C
-ON DC.CategoryID = C.CategoryID
-
-WHERE
-	T.NodeSiteID = 74
-	AND T.NodeAliasPath like '/Professional-Services%'
-
-Group by
-T.DocumentName, 
-	   [NodeAliasPath]
-	  ,DC.[CategoryID]
-	  ,C.[CategoryID]
-	  ,C.CategoryParentID
-	  ,C.CategoryDisplayName
-	  ,T.DocumentID
-	  ,C.CategoryNamePath
-	  ,C.CategoryLevel
-
-ORDER BY DocumentName, ParentCategory
